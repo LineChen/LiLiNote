6c1260f765c6e9cd6ff86a66b883bfb32f7a21f0
Beiing <15764230067@163.com>
2016-06-29 18:36:31 +0800

    Toolbar处理

---
+++ app/src/main/java/base/application/LiLiNoteApp.java
@@ -0,0 +1,16 @@
+package base.application;
+
+import android.app.Application;
+
+/**
+ * Created by chenliu on 2016/6/29.<br/>
+ * 描述：
+ * </br>
+ */
+public class LiLiNoteApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ lilineed/src/main/java/com/beiing/lilineed/JustLili.java
@@ -0,0 +1,9 @@
+package com.beiing.lilineed;
+
+/**
+ * Created by chenliu on 2016/6/29.<br/>
+ * 描述：
+ * </br>
+ */
+public class JustLili {
+}
